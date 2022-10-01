d22ed218208680928e2560576a8ea4e604a7184a
amyyne <tlijaniamine9@gmail.com>
2022-10-01 18:04:18 +0100

    add new cmd clt/seller component

---
+++ src/app/components/new-cmd-clt-frs/new-cmd-clt-frs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-cmd-clt-frs',
+  templateUrl: './new-cmd-clt-frs.component.html',
+  styleUrls: ['./new-cmd-clt-frs.component.scss']
+})
+export class NewCmdCltFrsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
