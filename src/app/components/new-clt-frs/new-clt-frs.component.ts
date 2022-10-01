723a54140b3bc4135ddb34821a66cc911bda7914
amyyne <tlijaniamine9@gmail.com>
2022-10-01 18:03:45 +0100

    add new clt/seller interface

---
+++ src/app/components/new-clt-frs/new-clt-frs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-clt-frs',
+  templateUrl: './new-clt-frs.component.html',
+  styleUrls: ['./new-clt-frs.component.scss']
+})
+export class NewCltFrsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
